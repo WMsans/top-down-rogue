dec826722825b3e680a5d2cef00b83735b82f35e
Jeremy <ggajy123@gmail.com>
2026-04-12 17:49:25 -0700

    feat(weapon): add visual_scene and trigger_visual() to base class

--- src/weapons/weapon.gd
+++ src/weapons/weapon.gd
@@ -2,6 +2,8 @@ class_name Weapon
 extends RefCounted
 
 var name: String = "Weapon"
+var visual_scene: PackedScene = null
+var visual: Node2D = null
 
 
 func use(_user: Node) -> void:
@@ -14,3 +16,12 @@ func tick(_delta: float) -> void:
 
 func is_ready() -> bool:
 	return true
+
+
+func trigger_visual(direction: Vector2) -> void:
+	if visual:
+		_do_visual(direction)
+
+
+func _do_visual(_direction: Vector2) -> void:
+	pass
