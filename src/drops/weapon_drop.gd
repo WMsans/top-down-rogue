25d92a1237c622ee4bb0ab5c5092d47ac9db27b0
Jeremy Zhao <jeremyzhao@Jeremys-Mini.attlocal.net>
2026-04-15 20:07:31 -0700

    fix: swap weapon drop

--- src/drops/weapon_drop.gd
+++ src/drops/weapon_drop.gd
@@ -21,11 +21,5 @@ func _pickup(player: Node) -> void:
 
 func _on_slot_selected(slot_index: int, player: Node) -> void:
 	var weapon_manager: WeaponManager = player.get_node("WeaponManager")
-	var old_weapon: Weapon = weapon_manager.swap_weapon(slot_index, weapon)
-	if old_weapon:
-		var drop_scene: PackedScene = preload("res://scenes/weapon_drop.tscn")
-		var new_drop: WeaponDrop = drop_scene.instantiate()
-		new_drop.weapon = old_weapon
-		player.get_parent().add_child(new_drop)
-		new_drop.global_position = player.global_position
+	weapon_manager.swap_weapon(slot_index, weapon)
 	queue_free()
