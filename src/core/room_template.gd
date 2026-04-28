4aace8fafe3d9f246083bfd6c00e5dbd0f42be89
Jeremy Zhao <jeremyzhao@Macmini.attlocal.net>
2026-04-27 20:55:50 -0700

    feat: add BiomeDef, RoomTemplate, PoolDef resources

---
+++ src/core/room_template.gd
@@ -0,0 +1,9 @@
+class_name RoomTemplate
+extends Resource
+
+@export var png_path: String = ""
+@export var weight: float = 1.0
+@export var size_class: int = 64
+@export var is_secret: bool = false
+@export var is_boss: bool = false
+@export var rotatable: bool = true
