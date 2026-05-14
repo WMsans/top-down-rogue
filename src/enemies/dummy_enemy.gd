d15be8063451600607f1a850b9bc09a0b61b6a58
Jeremy <ggajy123@gmail.com>
2026-05-14 14:12:11 -0700

    fix: dummy dummy

--- src/enemies/dummy_enemy.gd
+++ src/enemies/dummy_enemy.gd
@@ -4,20 +4,12 @@ extends Enemy
 
 func _ready() -> void:
 	super._ready()
-	_assign_default_weapon()
 	_setup_drop_table()
-	_set_base_modulate(Color(0.2, 0.8, 0.2))
-
-
-func _assign_default_weapon() -> void:
-	weapon = MeleeWeapon.new()
-	weapon.cooldown = 0.5
-	weapon.damage = 3.0
-	_attack_range = 28.0
-	speed = 60.0
-	max_health = 15
+	_set_base_modulate(Color(0.6, 0.6, 0.6))
+	speed = 0.0
+	_speed_base = 0.0
+	max_health = 30
 	health = max_health
-	_speed_base = speed
 
 
 func _setup_drop_table() -> void:
@@ -25,5 +17,4 @@ func _setup_drop_table() -> void:
 
 
 func _execute_attack() -> void:
-	if weapon and _player_ref and is_instance_valid(_player_ref):
-		weapon.use(self)
+	pass
