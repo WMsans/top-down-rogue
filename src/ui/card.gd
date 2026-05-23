61fbf040b40c3bfedddbddb0054daa93fdcab26a
Jeremy <ggajy123@gmail.com>
2026-05-22 17:13:17 -0700

    fix: card ugly frame

--- src/ui/card.gd
+++ src/ui/card.gd
@@ -3,7 +3,7 @@ extends Control
 
 signal card_clicked
 
-@export var card_size: Vector2 = Vector2(160, 220):
+@export var card_size: Vector2 = Vector2(160, 240):
 	set(v):
 		card_size = v
 		if is_node_ready():
